7b56edf4a24e07835a324c3b54602f455fb67141
vallinah <153283337+vallinah@users.noreply.github.com>
2025-03-24 21:29:55 +0300

    api rate config update

--- src/main/java/site/easy/to/build/crm/repository/TicketHistoRepository.java
+++ src/main/java/site/easy/to/build/crm/repository/TicketHistoRepository.java
@@ -1,9 +1,13 @@
 package site.easy.to.build.crm.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import site.easy.to.build.crm.entity.TicketHisto;
 
 @Repository
@@ -11,4 +15,12 @@ public interface TicketHistoRepository extends JpaRepository<TicketHisto, Intege
     public TicketHisto findById(int ticketId);
 
     public List<TicketHisto> findByCustomerCustomerId(int customerId);
+
+    @Query("SELECT t FROM TicketHisto t " +
+            "WHERE t.deleteAt IS NULL " +
+            "AND (:date1 IS NULL OR t.createdAt >= :date1) " +
+            "AND (:date2 IS NULL OR t.createdAt <= :date2)")
+    List<TicketHisto> getBetweenDate(
+            @Param("date1") LocalDateTime date1,
+            @Param("date2") LocalDateTime date2);
 }
--- src/main/java/site/easy/to/build/crm/service/ticket/TicketHistoServiceImpl.java
+++ src/main/java/site/easy/to/build/crm/service/ticket/TicketHistoServiceImpl.java
@@ -1,13 +1,15 @@
 package site.easy.to.build.crm.service.ticket;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
 import site.easy.to.build.crm.entity.TicketHisto;
 import site.easy.to.build.crm.repository.TicketHistoRepository;
 
-import java.util.List;
-
 @Service
-public class TicketHistoServiceImpl implements TicketHistoService{
+public class TicketHistoServiceImpl implements TicketHistoService {
     private final TicketHistoRepository ticketHistoRepository;
 
     public TicketHistoServiceImpl(TicketHistoRepository ticketHistoRepository) {
@@ -15,16 +17,23 @@ public class TicketHistoServiceImpl implements TicketHistoService{
     }
 
     @Override
-    public TicketHisto save(TicketHisto ticketHisto){
-        return  ticketHistoRepository.save(ticketHisto);
+    public TicketHisto save(TicketHisto ticketHisto) {
+        return ticketHistoRepository.save(ticketHisto);
     }
 
     @Override
-    public List<TicketHisto> findAll(){
+    public List<TicketHisto> findAll() {
         return ticketHistoRepository.findAll();
     }
 
     @Override
-    public TicketHisto findByTicketHistoId(int id){return ticketHistoRepository.findById(id);};
+    public TicketHisto findByTicketHistoId(int id) {
+        return ticketHistoRepository.findById(id);
+    };
+
+    @Override
+    public List<TicketHisto> getBetweenDate(LocalDateTime date1, LocalDateTime date2) {
+        return ticketHistoRepository.getBetweenDate(date1, date2);
+    }
 
 }
